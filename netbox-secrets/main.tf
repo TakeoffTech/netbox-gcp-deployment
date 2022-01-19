cd2d521e83abc416336ac913a7834c88e6b03f4e
Kevin Scheunemann <kevin@scheuk.com>
2022-01-19 17:43:24 -0500

    fix some logic around enabling auth and create secret for vouch jwt_secret

--- netbox-secrets/main.tf
+++ netbox-secrets/main.tf
@@ -1,14 +1,24 @@
 resource "random_password" "password" {
-  length           = 20
-  special          = true
+  length  = 20
+  special = true
+}
+
+resource "random_password" "jwt_secret" {
+  length  = 44
+  special = false
 }
 
 output "password" {
-  value = random_password.password.result
+  value     = random_password.password.result
   sensitive = true
 }
 
 output "sha1_password" {
-  value = sha1("${random_password.password.result}")
+  value     = sha1("${random_password.password.result}")
+  sensitive = true
+}
+
+output "jwt_secret" {
+  value     = random_password.jwt_secret.result
   sensitive = true
 }
