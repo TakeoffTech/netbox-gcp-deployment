e680204e0144c5be2a29ee61f778159ee0c25ce5
Kevin Scheunemann <kevin@scheuk.com>
2022-12-07 14:30:00 -0500

    add refresh_token grant type

--- terraform/modules/okta-application/main.tf
+++ terraform/modules/okta-application/main.tf
@@ -3,7 +3,7 @@ resource "okta_app_oauth" "netbox" {
   # label is the application name
   label                     = "${var.resource_prefix}${var.name}"
   type                      = "web"
-  grant_types               = ["authorization_code"]
+  grant_types               = ["authorization_code", "refresh_token"]
   redirect_uris             = var.redirect_uris
   post_logout_redirect_uris = var.post_logout_redirect_uris
   response_types            = ["code"]
